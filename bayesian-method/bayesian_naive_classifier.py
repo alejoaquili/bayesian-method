02d187c5145bd4d2d68edd8e01837765919c44ad
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-08-21 19:59:28 -0300

    Calculate class quantities

--- bayesian-method/bayesian_naive_classifier.py
+++ bayesian-method/bayesian_naive_classifier.py
@@ -9,26 +9,39 @@ def bayesian_classifier(training_matrix, matrix_to_classify):
     start_row = 0
     probabilities_for_classes = {}
     probabilities_of_classes = {}
+    class_quantity = get_class_quantity(sorted_matrix)
     for i in range(1, rows):
         if sorted_matrix[i][columns] != current_class:
             data_for_class = sorted_matrix[start_row:i, 0:columns]
-            probabilities_for_classes[current_class] = calculate_probabilities_for_class(data_for_class)
+            probabilities_for_classes[current_class] = calculate_probabilities_for_class(data_for_class, class_quantity)
             probabilities_of_classes[current_class] = (i - start_row) / rows
             start_row = i
             current_class = sorted_matrix[i][columns]
     data_for_class = sorted_matrix[start_row:rows, 0:columns]
-    probabilities_for_classes[current_class] = calculate_probabilities_for_class(data_for_class)
+    probabilities_for_classes[current_class] = calculate_probabilities_for_class(data_for_class, class_quantity)
     probabilities_of_classes[current_class] = (rows - start_row) / rows
     return classify(matrix_to_classify, probabilities_for_classes, probabilities_of_classes)
 
 
-def calculate_probabilities_for_class(data_for_class):
+def calculate_probabilities_for_class(data_for_class, class_quantity):
     columns = data_for_class.shape[1]
     rows = data_for_class.shape[0]
     probabilities = np.zeros(columns)
     for i in range(0, columns):
-        probabilities[i] = np.sum(data_for_class[:, i]) / rows
+        probabilities[i] = (np.sum(data_for_class[:, i]) + 1) / (rows + class_quantity)
     return probabilities
+
+
+def get_class_quantity(sorted_matrix):
+    rows = sorted_matrix.shape[0]
+    columns = sorted_matrix.shape[1]
+    class_quantity = 1
+    current_class = sorted_matrix[0][columns - 1]
+    for i in range(1, rows):
+        if sorted_matrix[i][columns - 1] != current_class:
+            current_class = sorted_matrix[i][columns - 1]
+            class_quantity += 1
+    return class_quantity
 
 
 def classify(matrix_to_classify, probability_for_classes, probability_of_class):
