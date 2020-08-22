a05abbd8c1cf7d3f54d7a2fdbad356f46bc41ddb
Alejo Aquili <aaquili@itba.edu.ar>
2020-08-22 00:54:27 -0300

    News reader.

--- bayesian-method/main.py
+++ bayesian-method/main.py
@@ -1,6 +1,10 @@
-from utils.data_loader import load_person_preferences
+import numpy as np
+from utils.data_loader import load_person_preferences, load_news_dataset, transform_news_dataset
 from bayesian_naive_classifier import bayesian_classifier
 
-titles, training_matrix = load_person_preferences("../data/PreferenciasBritanicos.csv")
-print(bayesian_classifier(training_matrix, [[1, 0, 1, 1, 0], [0, 0, 1, 1, 1]], [1, 1, 1, 1, 1]))
+#titles, training_matrix = load_person_preferences("../data/PreferenciasBritanicos.csv")
+#print(bayesian_classifier(training_matrix, [[1, 0, 1, 1, 0], [0, 0, 1, 1, 1]], [1, 1, 1, 1, 1]))
 
+titles, news_data = load_news_dataset("../data/Noticias_argentinas.xlsx")
+# transform_news_dataset(np.asarray([["Internacional"], ["Nacional"], ["Internacional"], ["Nacional"], ["Deportes"], ["Ciencia y Tecnología"], ["Nacional"], ["Nacional"], ["hola"], ["Economía"]]), ["Internacional", "Deportes", "Ciencia y Tecnología", "Economía"])
+transform_news_dataset(news_data, ["Internacional", "Deportes", "Ciencia y Tecnología", "Economía"], "../output/transformed_news.csv")
--- bayesian-method/utils/data_loader.py
+++ bayesian-method/utils/data_loader.py
@@ -1,7 +1,37 @@
+import numpy as np
 import pandas as pd
+import xlrd
 
 
 def load_person_preferences(file_path):
     matrix = pd.read_csv(file_path).to_numpy()
     titles = pd.read_csv(file_path, nrows=0).columns.tolist()
     return titles, matrix
+
+
+def load_news_dataset(file_path):
+    matrix = pd.read_excel(file_path).to_numpy()
+    matrix = matrix[:, 0:4]
+    # titles = pd.read_excel(file_path, nrows=0).columns.tolist()
+    # titles = titles[0:4]
+    titles = ['fecha', 'titular', 'fuente', 'categoria']
+    return titles, matrix
+
+
+def transform_news_dataset(matrix, news_categories, output_file=None):
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    processed_matrix = matrix
+    count = 0
+    for row in range(0, rows):
+        found = False
+        for category in news_categories:
+            if matrix[row][columns - 1] == category:
+                found = True
+        if not found:
+            processed_matrix = np.delete(processed_matrix, row - count, axis=0)
+            count += 1
+
+    if output_file is not None:
+        np.savetxt(output_file, np.asarray(processed_matrix, dtype=object), delimiter=",", fmt='%s')
+    return processed_matrix
