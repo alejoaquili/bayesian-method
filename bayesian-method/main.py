eee072ba18c896ad5b418eb4843e6b1f6f6db603
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-08-26 21:50:07 -0300

    Read binary.csv and discretize gre and gpa

--- bayesian-method/main.py
+++ bayesian-method/main.py
@@ -1,8 +1,13 @@
 import numpy as np
-from utils.data_loader import load_news_dataset, load_transformed_news_dataset
+from utils.data_loader import load_news_dataset, load_transformed_news_dataset, load_binary_dataset, discretize_binary_dataset
 from bayesian_naive_classifier import bayesian_classifier
 from models.news_classifier import NewsClassifier
 from sklearn.model_selection import train_test_split
+
+
+########################################################################################################################
+# Ej 2
+
 #titles, training_matrix = load_person_preferences("../data/PreferenciasBritanicos.csv")
 #print(bayesian_classifier(training_matrix, [[1, 0, 1, 1, 0], [0, 0, 1, 1, 1]], [1, 1, 1, 1, 1]))
 
@@ -11,29 +16,40 @@ from sklearn.model_selection import train_test_split
 #                       "../data/transformed_news.tsv")
 
 
-titles, news_data = load_transformed_news_dataset("../data/transformed_news.tsv")
-train_matrix = news_data[:, :3]
-categories = news_data[:, 3]
-training_news, test_news, training_target, test_target = train_test_split(train_matrix, categories, train_size=0.8,
-                                                                          stratify=categories)
-news_classifier = NewsClassifier()
-column = np.transpose
-extra_column = training_target.reshape(len(training_target), 1)
-training_news = np.append(training_news, extra_column, axis=1)
-news_classifier.train(training_news, ignore_words=True)
-# result = news_classifier.classify(test_news, test_target)
+########################################################################################################################
+# Ej 3
+
+# titles, news_data = load_transformed_news_dataset("../data/transformed_news.tsv")
+# train_matrix = news_data[:, :3]
+# categories = news_data[:, 3]
+# training_news, test_news, training_target, test_target = train_test_split(train_matrix, categories, train_size=0.8,
+#                                                                           stratify=categories)
+# news_classifier = NewsClassifier()
+# column = np.transpose
+# extra_column = training_target.reshape(len(training_target), 1)
+# training_news = np.append(training_news, extra_column, axis=1)
+# news_classifier.train(training_news, ignore_words=True)
+# # result = news_classifier.classify(test_news, test_target)
+# # print(result)
+# result, metrics = news_classifier.classify(test_news, test_target, generate_metrics=True, output_path="../output",
+#                                            use_sources=False, ignore_words=False)
 # print(result)
-result, metrics = news_classifier.classify(test_news, test_target, generate_metrics=True, output_path="../output",
-                                           use_sources=False, ignore_words=False)
-print(result)
-print(metrics.confusion_matrix)
-print(metrics.estimated_classifier_error)
-print(metrics.estimated_classifier_error_relative)
-print(metrics.true_positives_rate)
-print(metrics.false_positives_rate)
-print(metrics.recall)
-print(metrics.accuracy)
-print(metrics.precision)
-print(metrics.f1_score)
-print(metrics.matthews_correlation_coefficient)
-print(metrics.roc_point)
+# print(metrics.confusion_matrix)
+# print(metrics.estimated_classifier_error)
+# print(metrics.estimated_classifier_error_relative)
+# print(metrics.true_positives_rate)
+# print(metrics.false_positives_rate)
+# print(metrics.recall)
+# print(metrics.accuracy)
+# print(metrics.precision)
+# print(metrics.f1_score)
+# print(metrics.matthews_correlation_coefficient)
+# print(metrics.roc_point)
+
+
+########################################################################################################################
+# Ej 4
+
+titles, matrix = load_binary_dataset("../data/binary.csv")
+matrix = discretize_binary_dataset(titles, matrix)
+print(titles)
--- bayesian-method/utils/data_loader.py
+++ bayesian-method/utils/data_loader.py
@@ -41,3 +41,26 @@ def load_transformed_news_dataset(file_path):
     matrix = pd.read_csv(file_path, delimiter="\t").to_numpy()
     titles = ['fecha', 'titular', 'fuente', 'categoria']
     return titles, matrix
+
+
+def load_binary_dataset(file_path):
+    matrix = pd.read_csv(file_path).to_numpy()
+    titles = pd.read_csv(file_path, nrows=0).columns.tolist()
+    return titles, matrix
+
+
+def discretize_binary_dataset(titles, matrix):
+    # GRE ∈ {GRE ≥500, GRE < 500} y GPA ∈ {GPA ≥ 3, GPA < 3}.
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    discretized_matrix = np.zeros((rows, columns))
+    for row in range(0, rows):
+        for column in range(0, columns):
+            if titles[column] == "gre":
+                discretized_matrix[row][column] = 0 if matrix[row][column] < 500 else 1
+            elif titles[column] == "gpa":
+                discretized_matrix[row][column] = 0 if matrix[row][column] < 3 else 1
+            else:
+                discretized_matrix[row][column] = matrix[row][column]
+    return discretized_matrix
+
